cd18368caa50d154c33eac1e45a491d8206404ae
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-12-06 02:01:07 +0100

    refactoring project

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,8 @@
-import Footer from "./footer"
-import Meta from "./meta"
+
 
 export default function Layout({ preview, children }) {
 	return (
 		<>
-			<Meta />
 			<div className="min-h-screen">
 				<main>{children}</main>
 			</div>
